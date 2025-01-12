38a12ed55382aca425cf051a0cc5412735f45ac0
SeungHyun KIM <ksc981221@gmail.com>
2025-01-12 21:32:38 +0900

    김승현 : 0112 : 메인작업

--- workspace/webapp/assets/js/basic/headerMember.js
+++ workspace/webapp/assets/js/basic/headerMember.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // 내정보 모니터 구현
 let monitorLevel = 0;
 
@@ -24,3 +25,25 @@ function updateMonitorLevel() {
 }
 
 setInterval(updateMonitorLevel, 1000); // 매 1초마다 증가
+=======
+// 기여도 m
+let monitorLevel = 0;
+// 모디터 인치
+let monitorInches = 0;
+
+function increaseMonitorLevel() {
+  monitorLevel++; //증가
+  document.getElementById("monitor-level").textContent =
+  `${monitorLevel}m`; // 모니터 값 업뎃
+
+// 100m 시 1inch
+  if (monitorLevel >= 100) {
+  monitorInches++;
+  monitorLevel = 0; //초기화
+  document.getElementById("monitor-levelup").textContent =
+  `${monitorInches} inch`;
+  }
+}
+
+document.getElementById("user-name").textContent = "지지베베님";
+>>>>>>> Stashed changes
