b1d5d2cd93614d73433e17841867d5bd4d7d99fb
K-eunhye <11233211@naver.com>
2025-01-15 02:10:30 +0900

    은혜 : 0115 css작업 추가

--- workspace/webapp/assets/js/article/sec/askDetail.js
+++ workspace/webapp/assets/js/article/sec/askDetail.js
@@ -2,6 +2,7 @@ const tagBtns = document.querySelectorAll('.mjh-askDetail-tag-btn');
 
 tagBtns.forEach((tagBtn) => {
   tagBtn.addEventListener('click', function () {
+<<<<<<< HEAD
     // 모든 버튼에서 bjs-ask-select 클래스 제거
     tagBtns.forEach((btn) => btn.classList.remove('mjh-askDetail-select'));
 
@@ -12,11 +13,22 @@ tagBtns.forEach((tagBtn) => {
 //모니터 버튼 누르면 1증가
 
 let count = -1;
+=======
+    // 모든 버튼에서 mjh-askDetail-select 클래스 제거
+    tagBtns.forEach((btn) => btn.classList.remove('mjh-askDetail-select'));
+
+    // 클릭된 버튼에 mjh-askDetail-select 클래스 추가
+    tagBtn.classList.add('mjh-askDetail-select');
+  });
+});
+let count = 0;
+>>>>>>> keh
 
 const img = document.getElementById('Detail-moniter-img')
 const countplus = document.getElementById('Detail-monitercount')
 
 img.addEventListener('click',function(){
+<<<<<<< HEAD
   if(count===-1){
     count = 1;
   }else{
@@ -25,12 +37,26 @@ img.addEventListener('click',function(){
   countplus.textContent = parseInt(countplus.textContent)+count;
 })
 
+=======
+  if(count===0){
+    count++;
+  }else{
+    count--;
+  }
+  countplus.textContent = count;
+})
+
+
+>>>>>>> keh
 function Dropdown() {
   const menu = document.getElementById('dropdownMenu');
   menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> keh
 // 댓글 수 초기화는 0으로 해야되는데 html에 더미 댓글 하나 있어서
 // 초기 세팅을 1로 설정해뒀습니다~
 // 나중에 바꿔주시면 됩니다!
@@ -50,7 +76,11 @@ function addComment() {
 
     // 댓글 작성자
     const commmentAuthor = document.createElement('span');
+<<<<<<< HEAD
     commmentAuthor.classList.add('mjh-askDetail-comment-author');
+=======
+    commmentAuthor.classList.add('mjh-askDetail-commnet-author');
+>>>>>>> keh
     commmentAuthor.textContent = "3inch 임초현🏅"; // 나중에 불러오는거 여기 추가
 
     // 댓글 (진짜)
--- workspace/webapp/assets/js/article/sec/tipDetail.js
+++ workspace/webapp/assets/js/article/sec/tipDetail.js
@@ -48,7 +48,11 @@ function addComment() {
 
     // 댓글 작성자
     const commmentAuthor = document.createElement('span');
+<<<<<<< HEAD
     commmentAuthor.classList.add('mjh-tipDetail-comment-author');
+=======
+    commmentAuthor.classList.add('mjh-tipDetail-commnet-author');
+>>>>>>> keh
     commmentAuthor.textContent = "3inch 임초현🏅"; // 나중에 불러오는거 여기 추가
 
     // 댓글 (진짜)
